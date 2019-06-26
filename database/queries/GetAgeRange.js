26bb245f7e979261d138d5217370a46e32d4a6a7
christophe.seguinot <christophe.seguinot@gmail.com>
2019-06-26 19:32:25 +0200

    Implements min and max queries

--- database/queries/GetAgeRange.js
+++ database/queries/GetAgeRange.js
@@ -6,4 +6,13 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+    return Promise.all([
+        Artist.find({}).sort({ age: 1}).limit(1).then((artist) => artist[0].age),
+        Artist.find({}).sort({ age: -1}).limit(1).then((artist) => artist[0].age)
+    ]).then((result) => {
+        return {min: result[0], max: result[1]}
+    });
+    
+
+
 };
--- database/queries/GetYearsActiveRange.js
+++ database/queries/GetYearsActiveRange.js
@@ -6,4 +6,10 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
+    return Promise.all([
+        Artist.find({}).sort({ yearsActive: 1}).limit(1).then((artist) => artist[0].yearsActive),
+        Artist.find({}).sort({ yearsActive: -1}).limit(1).then((artist) => artist[0].yearsActive)
+    ]).then((result) => {
+        return {min: result[0], max: result[1]}
+    });
 };
